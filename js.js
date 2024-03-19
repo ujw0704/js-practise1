53f908660ce20a45ec8120b42ddda65edfcc3ee6
ujw0704 <ujwalsharma571@gmail.com>
2024-03-20 00:17:52 +0530

    new

--- js.js
+++ js.js
@@ -19,3 +19,43 @@ let newword = ""
     }
  }
  console.log(wrd(bigword))
+
+
+ // what will be the value in this
+ const {
+    a= "default",
+    b= "default",
+    c= "default",
+    d= "default",
+    e= "default",
+
+ }={
+    a:null,
+    b:undefined,
+    c: false,
+    d:0
+ }
+ console.log(a,b, c,d,e)
+
+//  console(5+true) explicate changes in js 
+
+let z =5, y=10
+let x =(++z>10 && ++y >5)
+console.log(z , y, x)
+// x will be false because and opeartor will only look check one condition if its true it want go for second argument so x is false
+
+console.log(null == 0)// false
+
+console.log(null>0)// false
+console.log(null>=0)// true
+// is given true when is compared with undfined
+// null is true is bcuse is equal to 0
+
+let p =10 ,m = 10 ,v =10
+
+if(p==m==v){
+    console.log("hello")
+// a=b true  value+1 +10 11 = 10 false so it bye
+}else{
+    console.log("bye")
+}
