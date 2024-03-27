f6d49bd0f439bf85e14443558b2f1466758f0e83
ujw0704 <ujwalsharma571@gmail.com>
2024-03-27 23:32:21 +0530

    few

---
+++ objct.js
@@ -0,0 +1,9 @@
+// math;
+
+// object has many propreties
+
+const mynewob= {username:"ujwal"}
+
+console.log(mynewob)
+
+// The only objects that don't inherit from Object.prototype are those with null prototype, or descended from other null prototype objects.
