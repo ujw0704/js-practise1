9cecae15866d403725b5a0a553c262414a63622c
ujw0704 <ujwalsharma571@gmail.com>
2024-04-09 22:34:00 +0530

    few

--- hof.js
+++ hof.js
@@ -2,9 +2,9 @@
 
 const arr = [1, 2,3, 4,5,6]
 
-// for(const array of arr){
-//     console.log(array)
-// }
+for(const array of arr){
+    console.log(array)
+}
 
 
 const greet = "hello world"
@@ -25,3 +25,11 @@ const value = coding.forEach((item)=>{
     console.log(item)
 
 })
+
+const  n = [1, 2, 3, 4, 5]
+
+    if(n.length<4){
+    const np=  n.slice(0,3)
+    console.log(np)
+    }
+
