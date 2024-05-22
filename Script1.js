4a27ef22d25d751780b0d52a15a9f02c19bfaf5b
ujw0704 <ujwalsharma571@gmail.com>
2024-05-22 21:25:52 +0530

    few

--- Script1.js
+++ Script1.js
@@ -1,23 +1,23 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const id = document.getElementById("title");
-//     console.log(id);
-// });
+document.addEventListener("DOMContentLoaded", function() {
+    const id = document.getElementById("title");
+    console.log(id);
+});
 
  
 document.addEventListener("DOMContentLoaded", function() {
     const myul = document.querySelectorAll("#oh");
     console.log(myul);
-    // const myLI = myul.getElementsByTagName("li"); // Get all <li> elements within the <ul>
-    // myLI[0].style.color = "red";
+    const myLI = myul.getElementsByTagName("li"); // Get all <li> elements within the <ul>
+    myLI[0].style.color = "red";
     myul.forEach(element => {
         element.style.backgroundColor = "blue"
         
-//     });
+    });
      // if want to use node list use foreach
     // Change the color of the first <li> element to green
 
     //if u want to covernt html or and thing we can use ARRAY.FROM()
-// });
+});
 
 
 //innerHTML example that includes both markup and text 
